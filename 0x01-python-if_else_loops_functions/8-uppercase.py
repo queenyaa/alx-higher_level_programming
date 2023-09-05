cbbe15c423b0ac4fbe4e3ff38cbea486f3824284
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-05 13:20:58 -0700

    print in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for ch in str:
+        if (ord(ch) >= ord('a')) and (ord(ch) <= ord('z')):
+            ch = chr(ord(ch)-ord('a')+ord('A'))
+        print("{}".format(ch), end='')
+    print()
