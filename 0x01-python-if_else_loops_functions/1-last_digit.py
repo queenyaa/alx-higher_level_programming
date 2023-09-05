bf9f0ef8d20666f9ceed4d68b8fdca98f534edf4
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-05 11:48:45 -0700

    modulus

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 
 # Extract the last digit of the number
-last_digit = abs(number) % 10
+if number < 0:
+    last_digit = number % (-10)
+else:
+    last_digit = number % 10
 
 # Determine the appropriate message based on the last digit
 if last_digit > 5:
