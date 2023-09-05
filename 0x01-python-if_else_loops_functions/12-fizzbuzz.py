c7cbff3547221711d6408a50670c24707a5973f6
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-05 14:39:51 -0700

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 
 def fizzbuzz():
     for e in range(1, 101):
-        if e % 3 == 0 and e % 5 == 0:
+        if e % 15 == 0:
             print("FizzBuzz ", end="")
         elif e % 3 == 0:
             print("Fizz " , end="")
