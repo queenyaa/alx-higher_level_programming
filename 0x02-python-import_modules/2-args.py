ddb95ca555da832361cdfc7da8497ab35f42767a
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-07 08:44:48 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
 
+from sys import argv
+arg_0 = "arguments."
+arg_1 = "argument:"
+arg_n = "arguments:"
+argc = len(argv) - 1
+
+def print_args():
+    for v in range(1, len(argv)):
+        print("{:d}: {}".format(v, argv[v]))
+
 if __name__ == "__main__":
-    import sys
+    if (argc == 0):
+        print("{:d} {}".format(argc, arg_0))
+    elif (argc == 1):
+        print("{:d} {}".format(argc, arg_1))
+        print_args()
+    else:
+        print("{:d} {}".format(argc, arg_n))
+        print_args()
 
-    num_args = len(sys.argv) - 1
+#if __name__ == "__main__":
+    #import sys
 
-    print("{} argument{}{}:".format(num_args, 's' if num_args != 1
-        else '', 's' if num_args != 1 else '.' if num_args == 0 else ':'))
+    #num_args = len(sys.argv) - 1
 
-    for v in range(1, len(sys.argv)):
-        print("{}: {}".format(v, sys.argv[v]))
+    #print("{} argument{}{}:".format(num_args, 's' if num_args != 1
+        #else '', 's' if num_args != 1 else '.' if num_args == 0 else ':'))
+
+    #for v in range(1, len(sys.argv)):
+        #print("{}: {}".format(v, sys.argv[v]))
