7c0d388a84dbf398d9eecbfb85b3a1d841d82d56
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-07 10:36:25 -0700

    mini calculator?

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    else:
+        a = int(argv[1])
+        operator = argv[2]
+        b = int(argv[3])
+        calculate(a, operator, b)
+
+if operator == '+':
+    print("{:d} {} {:d}".format(a, operator, b, add(a, b)))
+elif operator == '-':
+    print("{:d} {} {:d}".format(a, operator, b, sub(a, b)))
+elif operator == '*':
+    print("{:d} {} {:d}".format(a, operator, b, mul(a, b)))
+elif operator == '/':
+    print("{:d} {} {:d}".format(a, operator, b, div(a, b)))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
