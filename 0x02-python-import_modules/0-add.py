58391072e6bf50fcccd71b916c5bc67bb591b049
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-07 08:17:33 -0700

    arithmetical

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-#import the add function from add_0.py
+# import the add function from add_0.py
 if __name__ == "__main__":
-    """Print the sum of 1 and 2"""
     from add_0 import add
     # define variables a and b
     a = 1
     b = 2
 
     # Print the result
-    print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a,b)))
