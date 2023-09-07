525efe985987a64df683e5e684551ebcb36d9208
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-07 10:52:25 -0700

    calculs

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,20 +6,26 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
-    else:
-        a = int(argv[1])
-        operator = argv[2]
-        b = int(argv[3])
-        calculate(a, operator, b)
 
-if operator == '+':
-    print("{:d} {} {:d}".format(a, operator, b, add(a, b)))
-elif operator == '-':
-    print("{:d} {} {:d}".format(a, operator, b, sub(a, b)))
-elif operator == '*':
-    print("{:d} {} {:d}".format(a, operator, b, mul(a, b)))
-elif operator == '/':
-    print("{:d} {} {:d}".format(a, operator, b, div(a, b)))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    opera = argv[2]
+    g = {"+": add, "-": sub, "*": mul, "/":div}
+
+    if opera not in g:
+        print("Unkown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, opera, b, g[opera](a, b)))
+
+#if operator == '+':
+#    print("{:d} {} {:d}".format(a, operator, b, add(a, b)))
+#elif operator == '-':
+#    print("{:d} {} {:d}".format(a, operator, b, sub(a, b)))
+#elif operator == '*':
+#    print("{:d} {} {:d}".format(a, operator, b, mul(a, b)))
+#elif operator == '/':
+#    print("{:d} {} {:d}".format(a, operator, b, div(a, b)))
+#else:
+#    print("Unknown operator. Available operators: +, -, * and /")
+#    exit(1)
