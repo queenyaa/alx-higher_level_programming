34094b5b83ae668a33762d5b4d327156503c2739
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-07 07:37:45 -0700

    result of addition

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#import the add function from add_0.py
+from add_0 import add
+
+# define variables a and b
+a = 1
+b = 2
+
+# Calculate the result using add function
+res = add(a, b)
+
+# Print the result
+print("{} + {} = {}".format(a, b, res))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,5 @@
+Readme of 0x02-python-import_modules
+
+##Task 0##
+
+Prints out result of the addition of `1 + 2 = 3`
