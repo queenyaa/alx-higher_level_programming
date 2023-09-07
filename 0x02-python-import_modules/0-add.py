b1d8768a855fdcfcf0722eea9aec0510b256625e
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-07 07:39:55 -0700

    result of addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,13 +2,13 @@
 
 #import the add function from add_0.py
 from add_0 import add
+if __name__ == "__main__":
+    # define variables a and b
+    a = 1
+    b = 2
 
-# define variables a and b
-a = 1
-b = 2
+    # Calculate the result using add function
+    res = add(a, b)
 
-# Calculate the result using add function
-res = add(a, b)
-
-# Print the result
-print("{} + {} = {}".format(a, b, res))
+    # Print the result
+    print("{} + {} = {}".format(a, b, res))
