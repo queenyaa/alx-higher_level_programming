bfa889f70c61b574c5212cd43e849a77e0cab6be
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-08 05:58:01 -0700

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,11 +2,14 @@
 from sys import argv
 argx = len(argv) - 1
 
+
 def sum_arg():
+
     tot = 0
     for v in range(1, len(argv)):
         tot += int(argv[v])
     return tot
+
 
 if __name__ == "__main__":
     if (argx == 0):
