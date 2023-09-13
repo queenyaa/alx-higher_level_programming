bb2c14a9587256d96bea1ee0c6bb3ae07c85379f
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-13 13:00:45 -0700

    delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+
+    keys_del = [key for key, val in a_dictionary.items() if val == value]
+    for key in keys_del:
+        del (a_dictionary[key])
+    return (a_dictionary)
