8b7f6d9f8943fb3c9f8d19ad797a4e1e33d35f3b
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-25 13:17:32 -0700

    division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+
+    try:
+        res = a / b
+
+    except ZeroDivisionError:
+        res = None # Handle division by zero by stting result
+
+    finally:
+        print("Inside result: {}".format(res))
+        return (res)
