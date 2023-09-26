d04bd4bc968591ac20bf9b864d5ee93182a706c6
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-09-26 10:14:38 -0700

    squaring

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Module to define the Square class
+"""
+
+class Square:
+    """
+    This class represents a square.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Readme of 0x06-python-classes 
