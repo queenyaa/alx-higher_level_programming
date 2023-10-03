4595bc3d5f2a06c43bf4cd7a3c76ecead03947b5
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-02 20:58:38 -0700

    queen

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -95,12 +95,11 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
     if sys.argv[1].isdigit() is False:
-        print("N must be number")
+        print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
-
 
     bod = in_board(int(sys.argv[1]))
     solutions = rec_solve(bod, 0, 0, [])
