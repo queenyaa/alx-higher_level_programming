0489d5c1e5946f6a894a3d8a865bcb6c3b5c4457
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-03 19:14:54 -0700

    t

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-
-""" this module prints a string n times """
-
-
 def magic_string():
-    """function to print strings n times"""
-
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 1
-    else:
-        magic_string.count += 1
-    return ("BestSchool" + (", BestSchool" * (magic_string.count - 1)))
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".joint(["BestSchool" for x in range (magic_string.count)])
