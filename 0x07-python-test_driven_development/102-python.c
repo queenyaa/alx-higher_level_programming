abf1dcdd2a8bace0858ab71f1e9d9ac2fcf29576
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-06 10:04:15 -0700

    py

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,5 @@
 #include <Python.h>
+#include <stdio.h>
 
 /**
  * print_python_string - function to print Python strings
@@ -6,23 +7,34 @@
  */
 void print_python_string(PyObject *p)
 {
-	Py_ssize_t py_len;
-	char *str;
+	Py_UNICODE *value = PyUnicode_AsUnicode(p);
+	char *value = PyString_AsString(p);
 
-	printf("[.] string object info\n");
 	if (PyUnicode_Check(p))
 	{
-		py_len = PyUnicode_GET_LENGTH(p);
-		str = PyBytes_AsString(PyUnicode_AsUTF8String(p));
-		if (PyUnicode_IS_COMPACT_ASCII(p))
-			 printf("  type: compact ascii%s\n");
-		else
-			 printf("  type: compact unicode object%s\n");
-		printf("  length: %zd\n  value: %s\n", py_len, str);
+		printf("[.] string object info\n");
+		printf("  type: compact unicode object\n");
+		printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
+		
+		if (value)
+		{
+			wprintf(L"  value: %ls\n, value");
+		}
+	}
+	else if (PyString_Check(p))
+	{
+		printf("[.] string object info\n");
+		printf("  type: compact ascii\n");
+		printf("  length: %ld\n", PyString_GET_SIZE(p));
+
+		if (value)
+		{
+			printf("  value: %s\n", value);
+		}
 	}
 	else
 	{
+		printf("[.] string object info\n");
 		printf("  [ERROR] Invalid String Object\n");
 	}
-	fflush(stdout);
 }
