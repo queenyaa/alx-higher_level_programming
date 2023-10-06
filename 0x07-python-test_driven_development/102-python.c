0ed3decf086179ab83023d5939d716ee987dfe85
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-06 10:15:22 -0700

    py

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -38,3 +38,29 @@ void print_python_string(PyObject *p)
 		printf("  [ERROR] Invalid String Object\n");
 	}
 }
+
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	Py_Initialize();
+
+	/* Test cases */
+	print_python_string(PyUnicode_DecodeUTF8("The spoon does not exist", \
+				24, NULL));
+	print_python_string(PyUnicode_DecodeUTF8("ложка не \
+				существует", 19, NULL));
+	print_python_string(PyUnicode_DecodeUTF8("La cuillère n'existe pas", \
+				24, NULL));
+	print_python_string(PyUnicode_DecodeUTF8("勺子不存在", 5, NULL));
+	print_python_string(PyUnicode_DecodeUTF8("숟가락은 존재하지 \
+				않는다.", 14, NULL));
+	print_python_string(PyUnicode_DecodeUTF8("スプーンは存在しない", \
+				10, NULL));
+	print_python_string(NULL);  /* Invalid string object */
+
+	Py_Finalize();
+	return (0);
+}
