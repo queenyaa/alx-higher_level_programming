153296428be0b3e838e63f5498d40fda42eccf25
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-09 13:18:36 -0700

    area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Class of Geometry
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry"""
+
+    def area(self):
+        """Calculate the area. The method raises an exception."""
+        raise Exception("area() is not implemented")
