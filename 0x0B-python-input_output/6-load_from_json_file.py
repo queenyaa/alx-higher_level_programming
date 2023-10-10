43e768622197531c4e3bbacd96747d3644defb2f
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-10 10:37:13 -0700

    create

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+function that creates an object from a
+JSON file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file."""
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
