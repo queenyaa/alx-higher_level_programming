cad361acda7f30bbe5db69d242ab2141659452bd
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-11-14 15:50:46 -0800

    ts

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,3 @@
 -- Display the average temperature (F) by city ordered
--- by temperature in descending order
-SELECT city, AVG(value) AS avg_temp
-FROM temperature
-GROUP BY city
-ORDER BY avg_temp DESC;
+
+SELECT city, AVG(value) AS avg_temp FROM temperature GROUP BY city ORDER BY avg_temp DESC;
