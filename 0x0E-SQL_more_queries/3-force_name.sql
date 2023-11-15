fdba72627735711c93deae5e649543d40b0ae1b4
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-11-15 09:45:44 -0800

    0

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- Script that cretas a table on MySQL server
+
+-- Create the table force_name if it does not exist
+CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
