41cd9f48a73f734785553af221fa7254aa9375cf
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-11-15 11:01:13 -0800

    4

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -7,4 +7,4 @@ USE hbtn_0d_usa;
 SELECT *
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id;
+ORDER BY id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_usa;
 -- Select all cities with their corresponding names using join
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
+LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
