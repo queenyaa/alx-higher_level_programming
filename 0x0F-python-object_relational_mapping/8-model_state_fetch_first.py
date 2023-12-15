65275f9b7e50cbd419f8790321639cb757a3409e
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-15 12:09:39 -0800

    8 Terminal Saved Output.txt
    Terminal Saved Output.txt

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,12 +2,19 @@
 """
 Prints the first state object from the database hbtn_0e_6_usa
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
+    """
+    script takes 3 args, uses module SQLAlchemy
+    importing State and Base from model_state
+    """
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[[1], sys.argv[2], sys.argv[3]),
                             pool_pre_ping=True)
