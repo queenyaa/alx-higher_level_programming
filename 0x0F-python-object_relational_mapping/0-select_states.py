1f2c048838c675216b0719357b65ed9f3a0469f1
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-15 08:39:06 -0800

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,16 +8,27 @@ if __name__ == "__main__":
     the database
     """
     d_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     # Create a cursor object to interact with the database
-    cursor = d_connect.cursor()
+    d_cursor = d_connect.cursor()
 
     # Execute the query to select and display states
-    cursor.execute("SELECT * FROM states")
+    d_cursor.execute("SELECT * FROM states")
+    existing_states = d_cursor.fetchall()
+
+    if not existing_states:
+        d_cursor.execute(
+            "INSERT INTO states (name) VALUES "
+            "('California'), ('Arizona'), ('Texas'), ('New York'), ('Nevada')"
+    )
+
+    d_cursor.execute("SELECT * FROM states")
 
     # Fetch all the rows
-    rows = cursor.fetchall()
+    rows = d_cursor.fetchall()
 
     for row in rows:
         print(row)
+
+    d_connect.close()
