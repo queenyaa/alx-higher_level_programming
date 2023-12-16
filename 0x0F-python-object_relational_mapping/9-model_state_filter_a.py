52aedbc20143ce4554df89aed3fc71a0365a0dac
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-16 08:47:48 -0800

    9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -25,6 +25,6 @@ if __name__ == '__main__':
     session = Session()
 
     for state in session.query(State).order_by(State.id):
-        if 'a' in state.name:
+        if `a` in state.name:
             # for state in states:
             print('{}: {}'.format(state.id, state.name))
