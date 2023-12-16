6da07224a4fe782c4c0caf5c1f57073770b0a327
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-15 18:36:49 -0800

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
+Script that takes in the name of a state as an argument and
+lists all cities of the state
+"""
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+    """
+    script that takes 4 args, uses module MySQLdb
+    connects to a MySQL server running on localhost, port 3306
+
     if len(sys.argv) != 5:
         print("Usage: {} <username> <password> <state_name>".
               format(sys.argv[0]))
@@ -10,30 +20,25 @@ if __name__ == "__main__":
 
     username, password, database, state_name = sys.argv[1],
     sys.argv[2], sys.argv[3], sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-
-    query = """
-    SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ')
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
     """
 
-    cursor.execute(query, (state_name,))
-    result = cursor.fetchone()
-    if result and result[0]:
-        print(result[0])
-    else:
-        print("No cities found for the specified state.")
+    d_connect = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-    cursor.close()
-    db.close()
+    d_cursor = d_connect.cursor()
+
+    d_cursor.execute("SELECT cities.id, cities.name \
+                     FROM cities \
+                     JOIN states ON cities.state_id = states.id \
+                     WHERE states.name LIKE BINARY %(state_name)s \
+                     ORDER BY cities.id ASC", {'state_name': argv[4]})
+
+    rows = d_cursor.fetchone()
+
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
