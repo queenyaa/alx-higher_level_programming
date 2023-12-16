cb7ba56185d9d4b2ef929e7a00c38a11fdaaec9a
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-15 18:05:06 -0800

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python3
+"""
+Script to list all cities from database
+'hbtn_0e_4_usa'
+"""
+
 import MySQLdb
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
+    """
+    script takes 3 arfs, uses MySQLdb module
+    connects to a MySQL server running on localhost port 3306
+
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".
               format(sys.argv[0]))
         sys.exit(1)
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-
-    querry = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
     """
 
-    cursor.execute(query)
+    d_connect = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-    rows = cursor.fetchall()
+    d_cursor = d_connect.cursor()
 
-    for row in rows:
-        print(row)
+    d_cursor.execute("SELECT cities.id, cities.name, states.name\
+                     FROM cities JOIN states ON cities.state_id = states.id\
+                     ORDER BY cities.id ASC")
 
-    cursor.close()
-    db.close()
+    rows = d_cursor.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
