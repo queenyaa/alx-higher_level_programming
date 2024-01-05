a901ab21c698c173052565a856b7e56d39188f19
Yaa Serwaa Gyan <ysgyan@gmail.com>
2024-01-05 11:43:22 -0800

    8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # script to send a JSON POST request to a URL with the contents
-
-json_file="$2"
-
-if [ -f "$json_file" ]; then
-	curl -sX POST -H "Content-Type: application/json" -d @"$json_file" "$1"
-else
-	echo "Not a valid JSON file"
-fi
+curl -s -H "Content-Type: application/json" -d @"$2" "$1"
