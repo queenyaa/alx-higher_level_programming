b88b59696c17c8b4d5b71d255370bccb28db4f10
queenyaa <ysgyan@gmail.com>
2024-01-30 15:34:44 -0800

    get contents and store in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// Script tp get the contents of a webpage and stores in a file
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+      if (writeError) {
+        console.error(writeError);
+      } else {
+        console.log(`Body content successfully stored in ${filePath}`);
+      }
+    });
+  }
+});
