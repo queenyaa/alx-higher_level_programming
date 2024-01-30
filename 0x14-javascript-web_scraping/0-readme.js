ed49e4d4391fc006f7ef97619888f00be3e85afe
queenyaa <ysgyan@gmail.com>
2024-01-30 12:16:25 -0800

    now write into a file and create the file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -9,7 +9,7 @@ const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (error, data) => {
   if (error) {
     console.error(error);
-    } else {
-      console.log(data);
-    }
+  } else {
+    console.log(data);
+  }
 });
