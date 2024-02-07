13e4541329aec3ea8300674c9ea9f0e5a86fd585
queenyaa <ysgyan@gmail.com>
2024-02-07 10:38:33 -0800

    return a translation

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,14 +1,19 @@
-$(document).ready(function () {
-    function fetchTranslation(){
-        let languageCode = $('#language_code').val ();
-        $.get('https://www.fourtonfish.com/hellosalut/hello/', { lang: languageCode }, function(data){
-            $('#hello').text(data.hello);
-        });
-    }
-    $('#btn_translate').click(fetchTranslation);
-    $('#language_code').keypress(function (event) {
-        if (event.which === 13){
-            fetchTranslation();
-        }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+	function fetchTranslation() {
+		const lang = $('input#language_code').val();
+		$.get(
+			'https://hellosalut.stefanbohacek.dev/?lang=' + lang,
+        function (content) {
+			$('div#hello').text(content.hello);
+		},
+			'json'
+		);
+	}
+
+	$('input#btn_translate').on('click', fetchTranslation);
+	$('input#language_code').on('keypress', function (event) {
+		if (event.key === 'Enter') {
+			fetchTranslation();
+		}
+	});
 });
