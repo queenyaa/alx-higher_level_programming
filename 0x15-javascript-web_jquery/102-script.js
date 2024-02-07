37c56a3f14cfceff313372a12102fe37c1ac62ac
queenyaa <ysgyan@gmail.com>
2024-02-07 10:44:36 -0800

    return a translation

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,8 +1,18 @@
-$(document).ready(function(){
-    $('#btn_translate').click(function(){
-        let languageCode = $('#language_code').val();
-        $.get('https://www.fourtonfish.com/hellosalut/hello/', { lang: languageCode }, function(data){
-            $('#hello').text(data.hello);
-        });
+// Wait for the DOM to fully load
+document.addEventListener('DOMContentLoaded', function () {
+    // Event listener for the Translate button click
+    $('input#btn_translate').on('click', function () {
+      // Get the language code from the input field
+      const lang = $('input#language_code').val();
+      
+      // Fetch the translation using AJAX
+      $.ajax({
+        url: 'https://hellosalut.stefanbohacek.dev/?lang=' + lang,
+        dataType: 'json',
+        success: function (content) {
+          // Update the content of the hello div with the translation
+          $('div#hello').text(content.hello);
+        }
+      });
     });
-});
+  });
